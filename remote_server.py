fe5eb26a469fa338f9c561ebc833b0049e5d57fb
denis dombrovski <ddenis1994@gmailcom>
2019-12-21 16:27:46 +0200

    add DB support

--- remote_server.py
+++ remote_server.py
@@ -1,10 +1,17 @@
 from flask import Flask,request
 #flask restful api import
 from flask_restful import Resource,Api
+#flask mongoDB api import
+from flask_mongoengine import MongoEngine
+import DB.MongoRepresent as user
 
 app=Flask(__name__)
 #add restrgful for the progrem
 api=Api(app)
+app.config["MONGODB_SETTINGS"] = {
+	"db": "myapp",
+}
+db = MongoEngine(app);
 
 @app.route('/')
 def hello_world():
@@ -20,13 +27,27 @@ class LogIn(Resource):
 	def post(self):
 		username=request.form.get('username')
 		password=request.form.get('password')
-		return {'hello':request.form.get('username')}
+		print(username);
+		return {'result':True,
+				'code':123}
 
+class SaveFile(Resource):
+	def post(self):
+		test=user(username="test1",userUniq="1")
+		test.save()
+
+		return {
+			'result':True,
+			'code':0
+		}
 
 
 
 api.add_resource(
 	LogIn,
+	'/api/login')
+api.add_resource(
+	SaveFile,
 	'/api/login')
 
 
