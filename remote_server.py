a642c29c50a9ed70f1361b752389087554bcbbcf
denis dombrovski <ddenis1994@gmailcom>
2020-03-22 15:30:59 +0200

    add config file

--- remote_server.py
+++ remote_server.py
@@ -1,13 +1,15 @@
 import os
-import requests
+
+
 from flask import Flask
 from flask_login import LoginManager
-from authlib.flask.client import OAuth
-from models import db, Users
+
+from models import db
 from routesMap import routes_blueprint
 
 app = Flask(__name__)
-oauth=OAuth(app)
+
+
 
 login_manager = LoginManager()
 
@@ -19,10 +21,11 @@ def setup_app(app):
     def create_tables():
         db.create_all()
 
+    app.config.from_pyfile('config.py')
+
     db.init_app(app)
     login_manager.init_app(app)
 
-    app.config.from_pyfile('config.py')
     app.secret_key=os.urandom(24)
     # register the blueprint
     app.register_blueprint(routes_blueprint)
--- routesMap.py
+++ routesMap.py
@@ -1,37 +1,15 @@
-import crypt
-
-from Tools.scripts import google
-from flask import Blueprint, request, session
-
-from models import db, Users
-
 import json
 
-from google.oauth2 import id_token
-from google.auth.transport import requests
-
 import requests as normalRequests
+from authlib.oauth2 import OAuth2Error
+from flask import Blueprint, request, session
+from google.auth.transport import requests
+from google.oauth2 import id_token
 
-import base64
-import cryptography.exceptions
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
-
-GOOGLE_CLIENT_ID = "1029754518505-3u43tlle0uhqodueu4271n7rru15vdlf.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "3jA6N-qcK1W4NznlKGIQwW2Y"
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
-
-
-def get_google_provider_cfg():
-    return "https://accounts.google.com/o/oauth2/auth"
-
+from models import db, Users
+from OAuth2 import authorization
 
 routes_blueprint = Blueprint('example_blueprint', __name__)
-
 
 def current_user():
     if 'id' in session:
@@ -78,7 +56,7 @@ def register():
     }
 
 
-@routes_blueprint.route('/oauth/googleauthorize', methods=['GET', 'POST'])
+@routes_blueprint.route('/oauth/googleAuthorize', methods=['GET', 'POST'])
 def authorize():
     json_input = request.get_json()
     with open('./client_secret_google.json') as json_file:
@@ -89,7 +67,7 @@ def authorize():
         verify_url = "https://oauth2.googleapis.com/tokeninfo"
         params = {'id_token': json_input["idToken"]}
         r = normalRequests.get(url=verify_url, params=params)
-        print("key " + type(r.text))
+        print("debug data key " + r.text)
         # end debug#
 
         id_info = id_token.verify_oauth2_token(json_input["idToken"], requests.Request(), client_id)
@@ -98,8 +76,28 @@ def authorize():
         user_id = id_info['sub']
         print(user_id)
     except ValueError:
-
+        return {'result': False,
+         'code': 1}
         pass
 
     return {'result': True,
             'code': 0}
+
+
+@routes_blueprint.route('/oauth/authorize', methods=['GET', 'POST'])
+def authorizeNormal():
+    user = current_user()
+    if request.method == 'GET':
+        try:
+            grant = authorization.validate_consent_request(end_user=user)
+        except OAuth2Error as error:
+            return error.error
+        return False
+    if not user and 'username' in request.form:
+        username = request.form.get('username')
+        user = Users.query.filter_by(username=username).first()
+    if request.form['confirm']:
+        grant_user = user
+    else:
+        grant_user = None
+    return authorization.create_authorization_response(grant_user=grant_user)
